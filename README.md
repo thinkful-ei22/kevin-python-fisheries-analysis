1a69dcacd9293d0cbedf3e5b19d34c5f831e408c
Kevin <ketsang12@gmail.com>
2018-09-19 14:34:15 -0400

    fleshing out of potential features

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-  ## European Fisheries Depletion
+  ## European Fisheries Depletion Report -- 2006 - 2016
 
   I want to use data collected by fisheries to determine what fish species are being overfished.
--- main.py
+++ main.py
@@ -9,8 +9,8 @@ fishing = fishing[['Species', 'Area', 'Country',
 '2016', '2015', '2014', '2013', '2012', '2011', 
 '2010', '2009', '2008', '2007', '2006' ]]
 
-fishing = fishing.dropna(axis='rows')
-fishing_data = fishing[(fishing != 0).all(1)]
+fishing_data = fishing.dropna(axis='rows')
+fishing_data = fishing_data[(fishing != 0).all(1)]
 fishing_test = fishing_data[8:14]
 fishing_test['Depleted'] = pd.Series('No', index=fishing.index)
 # values each year are in the units of TLW -- tons of live weight
@@ -34,10 +34,16 @@ def location (fishing):
       # print('yes')
       fishing.at[index, 'Depleted'] = 'Yes'
 
+# str = input('Enter in the upper limit year')
+# print(str)
 
 location(fishing_test)
 print(tabulate(fishing_test, headers='keys', tablefmt='grid'))
-print(len(fishing_data))
-print(len(fishing))
+original_data = len(fishing)
+print('There are %d total entries in the 2006-2016 ICES Nominal Catch Dataset' % original_data)
+filtered_data = len(fishing_data)
+print('%d entries are being analyzed that contain non-zero values.' % filtered_data)
+
+#this discrepancy from 50,000+ to 7000 is likely due to some species of fish and shellfish being naturally rare/scarce in some waters
 
 # do analysis on the years 
